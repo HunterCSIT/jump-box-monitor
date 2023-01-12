afe5abe8be48bd1fcd39f91b1c39761c0418174b
Jon Stricoff <stricoff92@gmail.com>
2023-01-12 17:29:26 -0500

    wip

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 
+#!/usr/bin/python3
+
+
 from abc import ABC, abstractmethod
 import argparse
 import json
 import logging
 import hashlib
+import itertools
 import os
 import os.path
 import pathlib
@@ -11,17 +15,22 @@ import re
 import subprocess
 import sys
 import time
+import traceback
 import typing
+
+import psutil
+
 
 
 BASE_DIR = pathlib.Path(__file__).parent.absolute()
 DATA_DIR = os.path.join(BASE_DIR, "data")
+PROD_LOG_PATH = os.path.join(BASE_DIR, "warnings.log")
 
 ALERT_SNOOZE_TIME_SECONDS = 60 * 60 * 6
 
 # Resource usage max thresholds
 ALERT_MAX_MEMORY_USAGE_PERCENT = 0.33
-ALERT_MAX_CPU_USAGE_PERCENT = 0.33
+ALERT_MAX_CPU_USAGE_PERCENT = 0.01#0.45
 ALERT_MAX_DISK_USAGE_PERCENT = 0.33
 
 
@@ -44,7 +53,7 @@ def read_saved_value(key: str):
     except IOError as e:
         raise DataCacheMissError from e
     if data.get("expired_at"):
-        if data["expired_at"] > now_ts():
+        if data["expired_at"] < now_ts():
             os.remove(data_file_path)
             raise DataCacheMissError()
     return data['payload']
@@ -74,7 +83,7 @@ def run_shell_command(*command_parts):
 # Alert type definitions # # # # # #
 
 class AlertType:
-    PROCESS = "PROCESS"
+    PROCESSES = "PROCESSES"
     RESOURCES = "RESOURCES"
 
 class ResourceNames:
@@ -83,30 +92,36 @@ class ResourceNames:
     DISK = "DISK"
 
 class BaseAlert(ABC):
+
+    alert_type = NotImplemented
+
     @abstractmethod
     def to_key_name(self) -> str:
         pass
 
 class SystemResourceUsageAlert(BaseAlert):
 
+    alert_type = AlertType.RESOURCES
+
     def __init__(
         self,
         resource_name: str,
-        resource_usage: str,
-        resource_capacity: str
+        resource_usage_percent: float,
     ):
         self.resource_name = resource_name
-        self.resource_usage = resource_usage
-        self.resource_capacity = resource_capacity
+        self.resource_usage_percent = resource_usage_percent
 
     def to_key_name(self):
         return self.resource_name
 
     def __str__(self):
-        return f"<ResourceAlert {self.resource_name}>"
+        return f"<ResourceAlert {self.resource_name} @ {round(self.resource_usage_percent, 4)}>"
 
 
 class ProcessAlert(BaseAlert):
+
+    alert_type = AlertType.PROCESSES
+
     def __init__(
         self,
         process_id: str,
@@ -136,58 +151,91 @@ class ProcessAlert(BaseAlert):
         return md5sum(f"{self.process_id}-{self.user_uid}")
 
     def __str__(self):
-        return f"<ProcessAlert {self.process_id}>"
+        return f"<ProcessAlert pid{self.process_id} uid{self.user_uid}>"
 
 
 # # Check for resource alerts # # # # # #
 
-def _get_memory_usage() -> typing.List[int]:
-    total_usage_row = run_shell_command("free", "-t", "-m").split("\n")[-1]
-    if not total_usage_row.startswith("Total: "):
-        raise Exception("final row expected to start with 'Total: '")
-
-    # Replace consecutuve spaces with single space,
-    # Total_usage_row looks like this now 'Total: 17847 3370 9202'
-    total_usage_row = re.sub(r"\s+", " ", total_usage_row)
-    # Split total row into columns.
-    [total, used, free] = [
-        int(v) for v in total_usage_row.split(" ")
-        if v and re.match(r"^\d+$", v)
-    ]
-    return [total, used, free]
-
-def _check_cpu_usage() -> float:
-    # return cpu usage as a percentage
-    outText=run_shell_command("grep", "cpu", "/proc/stat")
-    print(outText)
-
 def get_system_resource_usage_alerts(logger) -> typing.List[SystemResourceUsageAlert]:
-    # check memory usage
-    [total_memory, used_memory, free_memory] = _get_memory_usage()
-    logger.debug(f"memory usage {[total_memory, used_memory, free_memory]}")
+    # Memory
+    memory_used_percent = psutil.virtual_memory().percent / 100
+    logger.info(f"memory_used_percent {memory_used_percent}")
+    # CPU
+    cpu_useage_percent = psutil.cpu_percent(interval=0.5) / 100
+    logger.info(f"cpu_useage_percent {cpu_useage_percent}")
+    # Disk
+    disk_useage_percent = psutil.disk_usage("/").percent / 100
+    logger.info(f"disk_useage_percent {disk_useage_percent}")
 
-    # CPU usage
-    cpu_useage = _check_cpu_usage()
-    logger.debug(f"cpu usage % {cpu_useage}")
 
+    alerts = []
+    if memory_used_percent > ALERT_MAX_MEMORY_USAGE_PERCENT:
+        alerts.append(SystemResourceUsageAlert(
+            ResourceNames.MEMORY,
+            memory_used_percent
+        ))
+        logger.warning(f"creating alert for MEMORY usage")
+    if cpu_useage_percent > ALERT_MAX_CPU_USAGE_PERCENT:
+        alerts.append(SystemResourceUsageAlert(
+            ResourceNames.CPU,
+            cpu_useage_percent
+        ))
+        logger.warning(f"creating alert for CPU usage")
+    if disk_useage_percent > ALERT_MAX_DISK_USAGE_PERCENT:
+        alerts.append(SystemResourceUsageAlert(
+            ResourceNames.DISK,
+            disk_useage_percent
+        ))
+        logger.warning(f"creating alert for DISK usage")
+
+    return alerts
 
 
 # # Check for processes alerts # # # # # #
 
 def get_processes_alerts(logger) -> typing.List[ProcessAlert]:
-    pass
+    return []
 
 
 
 # # # Script Entry Point # # # # # # # # #
 
-def main(debug: bool, logger: logging.Logger):
+def main(logger: logging.Logger) -> None:
+    logger.debug("main method running")
+
+    logger.debug("checking for alerts")
     resource_alerts = get_system_resource_usage_alerts(logger)
     process_alerts = get_processes_alerts(logger)
+    if len(resource_alerts) == 0 and len(process_alerts) == 0:
+        logger.debug("no alerts found")
+        return
+
+    if len(resource_alerts):
+        logger.warning(f"found {len(resource_alerts)} resource alert(s)")
+    if len(process_alerts):
+        logger.warning(f"found {len(process_alerts)} process alert(s)")
+    for alert in itertools.chain(resource_alerts, process_alerts):
+
+        alert_cache_key = alert.to_key_name()
+        try:
+            # Check cache to see if we've sent an alert for this event recently.
+            # We don't want to spam alerts.
+            read_saved_value(alert_cache_key)
+        except DataCacheMissError:
+            send_alert = True
+            logger.debug(f"cache miss, writing key to cache: {alert_cache_key}")
+            write_saved_value(alert_cache_key, None, ALERT_SNOOZE_TIME_SECONDS)
+        else:
+            logger.debug("cache hit")
+            logger.warning(f"not sending alert {alert}, cache key found")
+            send_alert = False
+
+        if send_alert:
+            logger.warning(f" **** SENDING ALERT **** {alert}")
+
 
 
 if __name__ == "__main__":
-    debug = True
 
     # check if production. If prod = True, no console logging and send alerts
     #                      If prod = False, log to console, show alerts in console.
@@ -195,11 +243,27 @@ if __name__ == "__main__":
     parser.add_argument('-p', '--production', action='store_true')
     args = parser.parse_args()
 
+    # Assemble logger instance
     logger = logging.getLogger("jump-box-monitor")
     logger.setLevel(logging.DEBUG)
     if args.production:
-        logger.addHandler(logging.NullHandler())
+        handler = logging.FileHandler(PROD_LOG_PATH, mode="a")
+        handler.setLevel(logging.WARN)
+        formatting = logging.Formatter('%(asctime)s %(levelname)s: %(message)s')
+        handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s'))
     else:
-        logger.addHandler(logging.StreamHandler(sys.stdout))
+        handler = logging.StreamHandler(sys.stdout)
+        handler.setLevel(logging.DEBUG)
+        handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s'))
+    logger.addHandler(handler)
+    logger.debug("logger created")
 
-    main(debug, logger)
+    # Call main method and log any errors.
+    try:
+        main(logger)
+    except Exception as e:
+        logger.error(f"An Error Occured :(: {e}")
+        logger.error(traceback.format_exc())
+        raise
+    finally:
+        logger.debug("goodbye")
